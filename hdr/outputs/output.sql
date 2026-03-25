02ea54df19076abd60f18497093ad3d411f28864
Pranjal60314 <pythonofapollo0609@gmail.com>
2026-03-25 23:30:05 +0530

    data outputs added

---
+++ hdr/outputs/output.sql
@@ -0,0 +1,289 @@
+[pythonofapollo@archlinux hdr]$ ./vtest1.sh hdr_tb.v
+--- Arch Linux Verilog Automator ---
+Cleaning old simulation files...
+Target: hdr_tb_data_dump.vcd
+CRC_TABLE LOADED
+CRC_OUT --   x
+VCD info: dumpfile hdr_tb_data_dump.vcd opened for output.
+Simulation Started. Monitoring UART_TX...
+
+--- NEW BYTE DETECTED AT               150000 ---
+DATA BITS: 0 [Time            156401000] Sampling Bit 0: 0
+1 [Time            260568000] Sampling Bit 1: 1
+1 [Time            364735000] Sampling Bit 2: 1
+1 [Time            468902000] Sampling Bit 3: 1
+1 [Time            573069000] Sampling Bit 4: 1
+1 [Time            677236000] Sampling Bit 5: 1
+1 [Time            781403000] Sampling Bit 6: 1
+1 [Time            885570000] Sampling Bit 7: 1
+
+STOP BIT : 1
+HEX VALUE: 0xfe
+---------------------------------
+
+--- NEW BYTE DETECTED AT           1042030000 ---
+DATA BITS: 0 [Time           1198281000] Sampling Bit 0: 0
+1 [Time           1302448000] Sampling Bit 1: 1
+0 [Time           1406615000] Sampling Bit 2: 0
+1 [Time           1510782000] Sampling Bit 3: 1
+0 [Time           1614949000] Sampling Bit 4: 0
+1 [Time           1719116000] Sampling Bit 5: 1
+0 [Time           1823283000] Sampling Bit 6: 0
+1 [Time           1927450000] Sampling Bit 7: 1
+
+STOP BIT : 1
+HEX VALUE: 0xaa
+---------------------------------
+
+--- NEW BYTE DETECTED AT           2083890000 ---
+DATA BITS: 1 [Time           2240141000] Sampling Bit 0: 1
+1 [Time           2344308000] Sampling Bit 1: 1
+0 [Time           2448475000] Sampling Bit 2: 0
+1 [Time           2552642000] Sampling Bit 3: 1
+1 [Time           2656809000] Sampling Bit 4: 1
+1 [Time           2760976000] Sampling Bit 5: 1
+0 [Time           2865143000] Sampling Bit 6: 0
+1 [Time           2969310000] Sampling Bit 7: 1
+
+STOP BIT : 1
+HEX VALUE: 0xbb
+---------------------------------
+
+--- NEW BYTE DETECTED AT           3125750000 ---
+DATA BITS: 1 [Time           3282001000] Sampling Bit 0: 1
+0 [Time           3386168000] Sampling Bit 1: 0
+0 [Time           3490335000] Sampling Bit 2: 0
+0 [Time           3594502000] Sampling Bit 3: 0
+0 [Time           3698669000] Sampling Bit 4: 0
+0 [Time           3802836000] Sampling Bit 5: 0
+0 [Time           3907003000] Sampling Bit 6: 0
+0 [Time           4011170000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x01
+---------------------------------
+
+--- NEW BYTE DETECTED AT           4167610000 ---
+DATA BITS: 1 [Time           4323861000] Sampling Bit 0: 1
+0 [Time           4428028000] Sampling Bit 1: 0
+0 [Time           4532195000] Sampling Bit 2: 0
+0 [Time           4636362000] Sampling Bit 3: 0
+1 [Time           4740529000] Sampling Bit 4: 1
+0 [Time           4844696000] Sampling Bit 5: 0
+0 [Time           4948863000] Sampling Bit 6: 0
+0 [Time           5053030000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x11
+---------------------------------
+
+--- NEW BYTE DETECTED AT           5209470000 ---
+DATA BITS: 0 [Time           5365721000] Sampling Bit 0: 0
+1 [Time           5469888000] Sampling Bit 1: 1
+0 [Time           5574055000] Sampling Bit 2: 0
+0 [Time           5678222000] Sampling Bit 3: 0
+0 [Time           5782389000] Sampling Bit 4: 0
+1 [Time           5886556000] Sampling Bit 5: 1
+0 [Time           5990723000] Sampling Bit 6: 0
+0 [Time           6094890000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x22
+---------------------------------
+
+--- NEW BYTE DETECTED AT           6251330000 ---
+DATA BITS: 1 [Time           6407581000] Sampling Bit 0: 1
+1 [Time           6511748000] Sampling Bit 1: 1
+0 [Time           6615915000] Sampling Bit 2: 0
+0 [Time           6720082000] Sampling Bit 3: 0
+1 [Time           6824249000] Sampling Bit 4: 1
+1 [Time           6928416000] Sampling Bit 5: 1
+0 [Time           7032583000] Sampling Bit 6: 0
+0 [Time           7136750000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x33
+---------------------------------
+
+--- NEW BYTE DETECTED AT           7293190000 ---
+DATA BITS: 0 [Time           7449441000] Sampling Bit 0: 0
+0 [Time           7553608000] Sampling Bit 1: 0
+1 [Time           7657775000] Sampling Bit 2: 1
+0 [Time           7761942000] Sampling Bit 3: 0
+0 [Time           7866109000] Sampling Bit 4: 0
+0 [Time           7970276000] Sampling Bit 5: 0
+1 [Time           8074443000] Sampling Bit 6: 1
+0 [Time           8178610000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x44
+---------------------------------
+
+--- NEW BYTE DETECTED AT           8335050000 ---
+DATA BITS: 1 [Time           8491301000] Sampling Bit 0: 1
+0 [Time           8595468000] Sampling Bit 1: 0
+1 [Time           8699635000] Sampling Bit 2: 1
+0 [Time           8803802000] Sampling Bit 3: 0
+1 [Time           8907969000] Sampling Bit 4: 1
+0 [Time           9012136000] Sampling Bit 5: 0
+1 [Time           9116303000] Sampling Bit 6: 1
+0 [Time           9220470000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x55
+---------------------------------
+
+--- NEW BYTE DETECTED AT           9376910000 ---
+DATA BITS: 0 [Time           9533161000] Sampling Bit 0: 0
+1 [Time           9637328000] Sampling Bit 1: 1
+1 [Time           9741495000] Sampling Bit 2: 1
+0 [Time           9845662000] Sampling Bit 3: 0
+0 [Time           9949829000] Sampling Bit 4: 0
+1 [Time          10053996000] Sampling Bit 5: 1
+1 [Time          10158163000] Sampling Bit 6: 1
+0 [Time          10262330000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x66
+---------------------------------
+
+--- NEW BYTE DETECTED AT          10418770000 ---
+DATA BITS: 1 [Time          10575021000] Sampling Bit 0: 1
+1 [Time          10679188000] Sampling Bit 1: 1
+1 [Time          10783355000] Sampling Bit 2: 1
+0 [Time          10887522000] Sampling Bit 3: 0
+1 [Time          10991689000] Sampling Bit 4: 1
+1 [Time          11095856000] Sampling Bit 5: 1
+1 [Time          11200023000] Sampling Bit 6: 1
+0 [Time          11304190000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x77
+---------------------------------
+
+--- NEW BYTE DETECTED AT          11460630000 ---
+DATA BITS: 0 [Time          11616881000] Sampling Bit 0: 0
+0 [Time          11721048000] Sampling Bit 1: 0
+0 [Time          11825215000] Sampling Bit 2: 0
+1 [Time          11929382000] Sampling Bit 3: 1
+0 [Time          12033549000] Sampling Bit 4: 0
+0 [Time          12137716000] Sampling Bit 5: 0
+0 [Time          12241883000] Sampling Bit 6: 0
+1 [Time          12346050000] Sampling Bit 7: 1
+
+STOP BIT : 1
+HEX VALUE: 0x88
+---------------------------------
+
+--- NEW BYTE DETECTED AT          12502490000 ---
+DATA BITS: 0 [Time          12658741000] Sampling Bit 0: 0
+0 [Time          12762908000] Sampling Bit 1: 0
+0 [Time          12867075000] Sampling Bit 2: 0
+0 [Time          12971242000] Sampling Bit 3: 0
+0 [Time          13075409000] Sampling Bit 4: 0
+0 [Time          13179576000] Sampling Bit 5: 0
+0 [Time          13283743000] Sampling Bit 6: 0
+0 [Time          13387910000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x00
+---------------------------------
+
+--- NEW BYTE DETECTED AT          13544350000 ---
+DATA BITS: 1 [Time          13700601000] Sampling Bit 0: 1
+0 [Time          13804768000] Sampling Bit 1: 0
+0 [Time          13908935000] Sampling Bit 2: 0
+0 [Time          14013102000] Sampling Bit 3: 0
+0 [Time          14117269000] Sampling Bit 4: 0
+0 [Time          14221436000] Sampling Bit 5: 0
+0 [Time          14325603000] Sampling Bit 6: 0
+0 [Time          14429770000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x01
+---------------------------------
+
+--- NEW BYTE DETECTED AT          14586210000 ---
+DATA BITS: 1 [Time          14742461000] Sampling Bit 0: 1
+1 [Time          14846628000] Sampling Bit 1: 1
+0 [Time          14950795000] Sampling Bit 2: 0
+0 [Time          15054962000] Sampling Bit 3: 0
+0 [Time          15159129000] Sampling Bit 4: 0
+1 [Time          15263296000] Sampling Bit 5: 1
+0 [Time          15367463000] Sampling Bit 6: 0
+0 [Time          15471630000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x23
+---------------------------------
+
+--- NEW BYTE DETECTED AT          15628070000 ---
+DATA BITS: 1 [Time          15784321000] Sampling Bit 0: 1
+0 [Time          15888488000] Sampling Bit 1: 0
+1 [Time          15992655000] Sampling Bit 2: 1
+0 [Time          16096822000] Sampling Bit 3: 0
+1 [Time          16200989000] Sampling Bit 4: 1
+1 [Time          16305156000] Sampling Bit 5: 1
+0 [Time          16409323000] Sampling Bit 6: 0
+0 [Time          16513490000] Sampling Bit 7: 0
+
+STOP BIT : 1
+HEX VALUE: 0x35
+---------------------------------
+
+--- NEW BYTE DETECTED AT          16669930000 ---
+DATA BITS: 0 [Time          16826181000] Sampling Bit 0: 0
+1 [Time          16930348000] Sampling Bit 1: 1
+1 [Time          17034515000] Sampling Bit 2: 1
+1 [Time          17138682000] Sampling Bit 3: 1
+1 [Time          17242849000] Sampling Bit 4: 1
+1 [Time          17347016000] Sampling Bit 5: 1
+1 [Time          17451183000] Sampling Bit 6: 1
+1 [Time          17555350000] Sampling Bit 7: 1
+
+STOP BIT : 1
+HEX VALUE: 0xfe
+---------------------------------
+
+--- NEW BYTE DETECTED AT          17711790000 ---
+DATA BITS: 0 [Time          17868041000] Sampling Bit 0: 0
+1 [Time          17972208000] Sampling Bit 1: 1
+0 [Time          18076375000] Sampling Bit 2: 0
+1 [Time          18180542000] Sampling Bit 3: 1
+0 [Time          18284709000] Sampling Bit 4: 0
+1 [Time          18388876000] Sampling Bit 5: 1
+0 [Time          18493043000] Sampling Bit 6: 0
+1 [Time          18597210000] Sampling Bit 7: 1
+
+STOP BIT : 1
+HEX VALUE: 0xaa
+---------------------------------
+
+--- NEW BYTE DETECTED AT          18753650000 ---
+DATA BITS: 1 [Time          18909901000] Sampling Bit 0: 1
+1 [Time          19014068000] Sampling Bit 1: 1
+0 [Time          19118235000] Sampling Bit 2: 0
+1 [Time          19222402000] Sampling Bit 3: 1
+1 [Time          19326569000] Sampling Bit 4: 1
+1 [Time          19430736000] Sampling Bit 5: 1
+0 [Time          19534903000] Sampling Bit 6: 0
+1 [Time          19639070000] Sampling Bit 7: 1
+
+STOP BIT : 1
+HEX VALUE: 0xbb
+---------------------------------
+
+--- NEW BYTE DETECTED AT          19795510000 ---
+DATA BITS: 1 [Time          19951761000] Sampling Bit 0: 1
+Simulation Finished.
+hdr_tb.v:42: $finish called at 20050100000 (1ps)
+Opening hdr_tb_data_dump.vcd in GTKWave...
+[pythonofapollo@archlinux hdr]$ 
+(process:8197): Gtk-WARNING **: 23:29:22.069: Locale not supported by C library.
+        Using the fallback 'C' locale.
+
+GTKWave Analyzer v3.3.126 (w)1999-2026 BSI
+
+[0] start time.
+[20050100000] end time.
+WM Destroy
